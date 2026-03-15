507ba764a448c7974a067dd7cb13e423fce3fe53
gavimg <gavigadagi@gmail.com>
2026-03-15 16:15:31 +0530

    Initial commit - adi-settings app

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import SettingsApp from './SettingsApp';
+
+const App: React.FC = () => <SettingsApp />;
+export default App;
---
+++ src/SettingsApp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SettingsApp: React.FC = () => {
+  return (
+    <div style={{ padding: '2rem', background: '#faf5ff', minHeight: '100vh' }}>
+      <h1>Settings Module</h1>
+      <p>This is the adi-settings micro-frontend.</p>
+    </div>
+  );
+};
+
+export default SettingsApp;
